567596b4e017710918416bf883dece3738f1a086
Natan-Aguiar <natantech@hotmail.com>
2022-02-18 19:54:36 -0300

    manutenção JS

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const card_effect = document.querySelectorAll(".card_effect");
 const main = document.querySelector(".main");
 
-card_effect[0].onclick = function corFundo0() {
-    main.style.background = getComputedStyle(card_effect[0]).background
-};
+card_effect[0].onclick = () => 
+    main.style.background = getComputedStyle(card_effect[0]).background;
 
-card_effect[1].onclick = function corFundo1() {
+card_effect[1].onclick = () => 
     main.style.background = getComputedStyle(card_effect[1]).background;
-};
 
-card_effect[2].onclick = function corFundo2() {
+card_effect[2].onclick = () => 
     main.style.background = getComputedStyle(card_effect[2]).background;
-};
