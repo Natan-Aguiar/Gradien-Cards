11187bf363fa08ff60c9394cc0c34b9ad5c48979
Natan-Aguiar <natantech@hotmail.com>
2022-02-21 21:41:18 -0300

    mudança no JS

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const card_effect = document.querySelectorAll(".card_effect");
 const main = document.querySelector(".main");
 
-card_effect[0].onclick = () => 
-    main.style.background = getComputedStyle(card_effect[0]).background;
-
-card_effect[1].onclick = () => 
-    main.style.background = getComputedStyle(card_effect[1]).background;
-
-card_effect[2].onclick = () => 
-    main.style.background = getComputedStyle(card_effect[2]).background;
+card_effect.forEach(function (item) {
+    item.onclick = () => 
+    main.style.background = getComputedStyle(item).background;      
+});
