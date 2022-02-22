789131856b4c06753ad50c233488aa0c46505069
Natan-Aguiar <natantech@hotmail.com>
2022-02-22 13:05:32 -0300

    aplicando clean code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const card_effect = document.querySelectorAll(".card_effect");
 const main = document.querySelector(".main");
 
-card_effect.forEach(function (item) {
-    item.onclick = () => 
-    main.style.background = getComputedStyle(item).background;      
+card_effect.forEach( (card_effect) => {
+    card_effect.onclick = () => 
+    main.style.background = getComputedStyle(card_effect).background;      
 });
