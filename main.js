60561b77aeade742a0055a212211e55659f257af
Natan-Aguiar <99612730+Natan-Aguiar@users.noreply.github.com>
2022-03-22 11:24:08 -0300

    refatorando

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-const card_effect = document.querySelectorAll(".card_effect");
-const main = document.querySelector("body");
-
-card_effect.forEach( (card_effect) => {
+document.querySelectorAll(".card_effect").forEach((card_effect)=> {
     card_effect.onclick = () => 
-    main.style.background = getComputedStyle(card_effect).background;      
+    document.querySelector("main").style.background = getComputedStyle(card_effect).background;      
 });
