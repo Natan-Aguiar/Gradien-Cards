19cd0e9a5f940b8c60f04a80b5c07d6ff1fd8df9
Natan-Aguiar <99612730+Natan-Aguiar@users.noreply.github.com>
2022-03-22 11:33:31 -0300

    clean

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.querySelectorAll(".card_effect").forEach((card_effect)=> {
-    card_effect.onclick = () => 
+document.querySelectorAll(".card_effect").forEach((card_effect)=> { //seleciona todas class e para cada um deles
+    card_effect.onclick = () =>                                     // retorna um evento
     document.querySelector("main").style.background = getComputedStyle(card_effect).background;      
 });
