68d6035fd214f041b522a34f21274a66d788a1e3
Natan-Aguiar <natantech@hotmail.com>
2022-03-30 10:41:58 -0300

    desempenho

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const main = document.querySelector("main");
 const myCards = document.querySelectorAll(".card_effect");
 
-myCards.forEach( myCards => {       // altera background do Main por cada cada card selecionado.
+myCards.forEach( myCards => {   // altera background do Main por cada card selecionado.
     myCards.onclick = () =>         
-    main.style.background = getComputedStyle(myCards).background;      
+    document.querySelector("main").style.background = getComputedStyle(myCards).background;      
 });
